90dc7b3be0310950cd518348555d96a0b6f22c88
Jose <jose@Joses-MacBook-Air.local>
2016-01-30 18:20:31 -0500

    Updated Menu class
    started working on menu

--- core/src/com/cultstoppers/MenuState.java
+++ core/src/com/cultstoppers/MenuState.java
@@ -1,7 +1,31 @@
 package com.cultstoppers;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Created by jose on 1/30/16.
  */
-public class MenuState {
+public class MenuState extends State {
+
+    Texture menu;
+    SpriteBatch batch;
+    boolean play;
+    boolean confirm;//when character is confirmed change play state
+
+    public MenuState(){
+        play = false;
+        confirm = false;
+    }
+
+    public void render() {
+        if(play){
+            //if play or start button is hit then switch to character select screen
+        }
+    }
+
+    public boolean startGame(){
+        return confirm;//character confirmed so change state
+    }
+
 }
