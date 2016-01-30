9bd514d8f9594611c341cc7e6a2afb59323b9d5a
leonliang33 <llian008@fiu.edu>
2016-01-30 13:32:38 -0500

    Add shotgun to UI

--- core/src/com/cultstoppers/UserInterface.java
+++ core/src/com/cultstoppers/UserInterface.java
@@ -8,25 +8,31 @@ import java.util.ArrayList;
  * Created by andresmoser on 1/29/16.
  */
 public class UserInterface{
-    int healthX, healthY, numHealth, roomName, roomNum;
+    int healthX, healthY, numHealth, roomName, roomNum, weaponX,weaponY;
     Texture healthIcon = new Texture("heart.png");
     SpriteBatch batch = new SpriteBatch();
     ArrayList<Texture> healthBar = new ArrayList<Texture>();
-
+    Texture shotgunTexture = new Texture("Shotgun.png");
+    Weapon playerWeapon;
 
     public UserInterface(int health){
         numHealth = health;
         healthX = Gdx.graphics.getWidth() / 30;
         healthY = Gdx.graphics.getHeight() - 50;
-        for(int i = 0; i < numHealth; i++)
+        weaponX = Gdx.graphics.getWidth() / 40;
+        weaponY = Gdx.graphics.getHeight() - 60 - healthX;
+        for(int i = 0; i < numHealth; i++) {
             healthBar.add(healthIcon);
-
+        }
     }
 
     public void render() {
         batch.begin();
         for(int i = 0; i < numHealth; i++)
             batch.draw(healthBar.get(i), (healthX*(i+1)), healthY);
+        batch.end();
+        batch.begin();
+            batch.draw(shotgunTexture,weaponX,weaponY);
         batch.end();
     }
 
