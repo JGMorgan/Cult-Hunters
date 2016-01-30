3a40313801ace6de44755202152a813cc7104cf6
Jose <jose@Joses-MacBook-Air.local>
2016-01-30 15:05:53 -0500

    pimpin purple
    the title says it all

--- core/src/com/cultstoppers/Game.java
+++ core/src/com/cultstoppers/Game.java
@@ -16,7 +16,7 @@ public class Game extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         state.render();
 	}
