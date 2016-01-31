454e31c29a323a75417cc4538602759b52c3a36a
Jose <jose@Joses-MacBook-Air.local>
2016-01-31 16:23:12 -0500

    Update

--- core/src/com/cultstoppers/Game.java
+++ core/src/com/cultstoppers/Game.java
@@ -5,15 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	State state;
 	char stateType;
+    SpriteBatch batch;
+    Texture texture;
+    boolean credits;
 	@Override
 	public void create () {
         state = new MenuState();
 		stateType = 'm';
+        batch = new SpriteBatch();
+        texture = new Texture("UI/CreditsPage.png");
+        credits = true;
+
     }
 
 	@Override
@@ -21,18 +29,39 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		if(state.changeState()){
+
 			if(stateType == 'm'){
                 state.music.dispose();
 				state = new PlayState();
                 stateType = 'p';
 			}else {
-                state.music.dispose();
-				state = new MenuState();
-                stateType = 'm';
+                if (credits) {
+                    if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+                        credits = false;
+                    }
+                    batch.begin();
+                    batch.draw(texture,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+                    batch.end();
+                }else {
+                    state.music.dispose();
+                    state = new MenuState();
+                    stateType = 'm';
+                }
 			}
 		}
 		if(state.changeState()){
-			state=new MenuState();
+            if (credits) {
+                if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+                    credits = false;
+                }
+                batch.begin();
+                batch.draw(texture,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+                batch.end();
+            }else {
+                state.music.dispose();
+                state = new MenuState();
+                stateType = 'm';
+            }
 		}else
         state.render();
 	}
