233069032c0ea3e58e439a8c3733e06f7d0f6a74
Jose <jose@Joses-MacBook-Air.local>
2016-01-30 20:14:08 -0500

    Map updates
    generating levels

--- core/src/com/cultstoppers/Map.java
+++ core/src/com/cultstoppers/Map.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.util.Random;
+
 /**
  * Created by jose on 1/30/16.
  */
@@ -11,12 +13,25 @@ public class Map {
     Texture tilesheet;
     TextureRegion[] tiles;
     SpriteBatch batch;
+    Random r;
     public Map(){
+        r = new Random();
+        tilesheet = new Texture("GroundTileStripArray.png");
+        TextureRegion tmp [][] = TextureRegion.split(tilesheet, tilesheet.getWidth()/7, 1);
+        for(int i = 0; i < 7; i++){
+            tiles[i]=tmp[i][0];
+        }
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 3; j++){
 
+            }
+        }
     }
     public void render(){
         batch.begin();
-        //batch.draw(texture, x, y);
+        for(int i = 0; i < tiles.length; i++) {
+            batch.draw(tiles[i], ((i)+1),(i+1));
+        }
         batch.end();
     }
 }
